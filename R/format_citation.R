30140c75abe340760d9dea80368eb89627560a09
brunomioto <brunomioto97@gmail.com>
2022-11-21 12:26:05 -0300

    Initial commit

---
+++ R/format_citation.R
@@ -0,0 +1,34 @@
+#' Format authors' names into ABNT
+#'
+#' @param variable The authors' names variable
+#' @param separator The separator used between authors' names
+#' @param space Is there space after the separator?
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' x <- data.frame(
+#' names = c("Daenerys Targaryen, George R. R. Martin, Luís Inácio Lula da Silva",
+#'           "Hadley Alexander Wickham, Joseph J. Allaire",
+#'           "Stack Overflow"))
+#' x %>%
+#'   dplyr::mutate(new_names = format_names(names))
+
+format_citation <- function(variable, separator = ",", space = TRUE) {
+
+  sep <- paste0({{separator}},
+         if({{space}} == TRUE){
+           " "
+         }
+  )
+
+  variable %>%
+    strsplit(sep) %>%
+    sapply(. %>%
+             gsub("(.*?) (\\w+$)", "\\U\\2\\E, \\1", ., perl = TRUE) %>%
+             gsub(" ([A-Z])\\w*\\.?", " \\1.", .) %>%
+             paste(collapse = "; "))
+
+}
+
